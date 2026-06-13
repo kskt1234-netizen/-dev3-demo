277f9e956714cd0611ce9c2de62c73d01b42f83f
kskt1234-netizen <kskt1234@naver.com>
2026-06-13 18:35:08 +0900

    feat: add order service

---
+++ order_service.py
@@ -0,0 +1,9 @@
+﻿from models import Order, Product
+
+def get_order_details(order_ids: list):
+    orders = []
+    for order_id in order_ids:
+        order = Order.objects.get(id=order_id)
+        product = Product.objects.get(id=order.product_id)
+        orders.append({"order": order, "product": product})
+    return orders
