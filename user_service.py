1bc642521df25afebdbe874f61f90e9a34346766
kskt1234-netizen <kskt1234@naver.com>
2026-06-13 18:34:55 +0900

    feat: add user service

---
+++ user_service.py
@@ -0,0 +1,9 @@
+﻿from database import db
+
+def get_user(user_id: str):
+    query = f"SELECT * FROM users WHERE id = '{user_id}'"
+    return db.execute(query)
+
+def get_orders(status: str):
+    query = f"SELECT * FROM orders WHERE status = '{status}'"
+    return db.execute(query)
